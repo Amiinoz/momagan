692be78b89bc898c07010d966200c8e57f32d742
Amiinoz <momagan@gmail.com>
2020-06-10 23:51:38 -0600

    add gsap and relax

--- src/script/main.js
+++ src/script/main.js
@@ -10,3 +10,30 @@ const showMenu = (toggleId, navId) => {
   }
 };
 showMenu("nav-toggle", "nav-menu");
+
+// bring relax
+const rellax = new Rellax(".parallax");
+
+// gsap nav animation
+gsap.from(".nav__logo", {
+  opacity: 0,
+  duration: 3,
+  delay: 0.5,
+  y: 30,
+  ease: "expo.out",
+});
+gsap.from(".nav__toggle", {
+  opacity: 0,
+  duration: 3,
+  delay: 0.7,
+  y: 30,
+  ease: "expo.out",
+});
+gsap.from(".nav__item", {
+  opacity: 0,
+  duration: 3,
+  delay: 0.7,
+  y: 35,
+  ease: "expo.out",
+  stagger: 0.2,
+});
